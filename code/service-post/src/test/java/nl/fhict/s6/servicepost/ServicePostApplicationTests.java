99563c7253aa996d8d1976010035a9cebfb1b1dd
Cas <407529@student.fontys.nl>
2021-05-30 20:36:08 +0200

    added intergration tests

--- code/service-post/src/test/java/nl/fhict/s6/servicepost/ServicePostApplicationTests.java
+++ code/service-post/src/test/java/nl/fhict/s6/servicepost/ServicePostApplicationTests.java
@@ -3,11 +3,20 @@ package nl.fhict.s6.servicepost;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @SpringBootTest
 class ServicePostApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	}
+	@Test
+	void load()
+	{
+		assertTrue(false);
 	}
 
 }
